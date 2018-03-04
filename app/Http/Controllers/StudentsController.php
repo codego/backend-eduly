e8e3aa409faf546e497265f7b83e58a690899587
codego <joaquin.ditoma@trocafone.com>
2018-03-04 19:01:11 -0300

    init commit

---
+++ app/Http/Controllers/StudentsController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\students;
+use Illuminate\Http\Request;
+
+class StudentsController extends Controller
+{
+
+    public function __construct()
+    {
+        // $this->middleware('auth:api');
+    }
+
+    public function show($id)
+    {
+        //return new StudentsResource(Students::find($id));
+        //return Students::where('id', $id)->get();
+        //return response()->json(array('success' => true), 200);
+        return Students::find($id);
+
+    }
+
+    public function showAll(Request $request)
+    {
+        $sort = $request->input('_sort');
+        $end = $request->input('_end');
+        $start = $request->input('_start');
+        $order = $request->input('_order');
+        return response(DB::table('students')->orderBy($sort, $order)->offset($start)->limit($end)->get(), 200)
+                ->header('X-Total-Count', \App\Students::all()->count());
+    }
+
+    public function delete($id) {
+        $student = Students::find($id);
+        $student->delete();
+        return response()->json(array('success' => true), 200);
+    }
+
+    public function edit($id) {
+        $student = Students::find($id);
+
+        $studentData = json_decode(request()->getContent(), true);
+
+        $student->name = $studentData['name'];
+        $student->lastname = $studentData['lastname'];
+        $student->document = $studentData['document'];
+        $student->degree = $studentData['degree'];
+        $student->gender = $studentData['gender'];
+        $student->phone = $studentData['phone'];
+        $student->address = $studentData['address'];
+        $student->locality = $studentData['locality'];
+        $student->city = $studentData['city'];
+        $student->province = $studentData['province'];
+        $student->place_birth = $studentData['place_birth'];
+        $student->place_residence = $studentData['place_residence'];
+        $student->email = $studentData['email'];
+        $student->status = $studentData['status'];
+        $student->entry = date('Y-m-d H:i:s'); //$studentData['entry'];
+        //$student->egress = $studentData['egress'];
+
+        $student->save();
+        return response()->json(array('success' => true, 'id' => $student->id), 200);
+
+
+    }
+
+    public function create(Request $request)
+    {
+        $student = new Students;
+        $studentData = json_decode(request()->getContent(), true);
+
+        $student->name = $studentData['name'];
+        $student->lastname = $studentData['lastname'];
+        $student->document = $studentData['document'];
+        $student->degree = $studentData['degree'];
+        $student->gender = $studentData['gender'];
+        $student->phone = $studentData['phone'];
+        $student->address = $studentData['address'];
+        $student->locality = $studentData['locality'];
+        $student->city = $studentData['city'];
+        $student->province = $studentData['province'];
+        $student->place_birth = $studentData['place_birth'];
+        $student->place_residence = $studentData['place_residence'];
+        $student->email = $studentData['email'];
+        $student->status = $studentData['status'];
+        $student->entry = date('Y-m-d H:i:s'); //$studentData['entry'];
+        //$student->egress = $studentData['egress'];
+
+        $student->save();
+        return $student->id;
+    }
+}
---
+++ database/migrations/2018_02_27_030309_create_students_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id')->index();;
+            $table->string('name');
+            $table->string('lastname');
+            $table->string('document');
+            $table->string('degree')->nullable();
+            $table->string('gender');
+            $table->string('phone')->nullable();
+            $table->string('address');
+            $table->string('locality');
+            $table->string('city');
+            $table->string('province');
+            $table->string('place_birth');
+            $table->string('place_residence');
+            $table->string('email');
+            $table->integer('status');
+            $table->date('entry');
+            $table->date('egress')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
