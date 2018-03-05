bb553b600fc261ae8dc67d0c724225b59b4a671a
codego <joaquin.ditoma@trocafone.com>
2018-03-04 21:20:27 -0300

    create file cors.php

---
+++ config/cors.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS
+    |--------------------------------------------------------------------------
+    |
+    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+    | to accept any value.
+    |
+    */
+
+    'supportsCredentials' => false,
+    'allowedOrigins' => ['*'],
+    'allowedOriginsPatterns' => [],
+    'allowedHeaders' => ['*'],
+    'allowedMethods' => ['*'],
+    'exposedHeaders' => ['Content-Disposition', 'X-Total-Count', 'x-filename', '*'],
+    'maxAge' => 0,
+
+];
