c7bacf91bf856c51bf35ef47d9780e975e676bd3
codego <joaquin.ditoma@trocafone.com>
2018-03-18 21:51:08 -0300

    debug

--- app/Services/CorrelativeService.php
+++ app/Services/CorrelativeService.php
@@ -9,8 +9,9 @@ class CorrelativeService
 {
     public function store($id_subject, $ids_subjects_dependences)
     {
-        DB::table('correlatives')->whereIn('id_subject', $id_subject)->delete();
         var_dump($ids_subjects_dependences);
+        var_dump($id_subject);
+        //  DB::table('correlatives')->whereIn('id_subject', $id_subject)->delete();
         dd();
     }
 }
